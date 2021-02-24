510d24c2ef98aac92f229f92769a0ed8361d709c
Yasya Mashnenko <yasyamashnenko@MacBook-Air-Daniil.local>
2021-02-24 15:58:26 +0100

    create single component for utility arrays

--- src/components/Utilities.js
+++ src/components/Utilities.js
@@ -9,9 +9,23 @@ const imagesForCards = [
   { src: "https://picsum.photos/200/250?random=4" },
 ];
 
+// const imagesForShortGame = imagesForCards
+//   .splice(4, 4)
+//   .sort(() => Math.random() - 0.5)
+//   .forEach((item) => (item.id = Math.round(Math.random() * 10000)));
+// console.log(imagesForShortGame);
+
+// const imagesForLongGame =
 imagesForCards
   .sort(() => Math.random() - 0.5)
-  .forEach((item) => (item.id = Math.round(Math.random() * 100)));
-console.log(imagesForCards);
+  .forEach((item) => (item.id = Math.round(Math.random() * 10000)));
 
-export default imagesForCards;
+const menuList = [
+  "New Game",
+  "Short Game",
+  "Sound",
+  "Play with Time",
+  "Dark Theme",
+];
+
+export { menuList, imagesForCards };
