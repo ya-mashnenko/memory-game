2606571ef95405ca11f40474996886b200764e9f
Yasya Mashnenko <yasyamashnenko@MacBook-Air-Daniil.local>
2021-02-25 14:27:37 +0100

    add draft for theme mode

--- src/components/App.js
+++ src/components/App.js
@@ -1,17 +1,33 @@
-import React from "react";
+import React, { useState } from "react";
 import Footer from "./Footer";
 import Header from "./Header";
 import GameBoard from "./GameBoard";
 import Modal from "./Modal";
+import { ThemeProvider } from "styled-components";
+import { lightTheme, darkTheme } from "./Theme";
+import { GlobalStyles } from "./Style";
 
 const App = () => {
+  // const [theme, setTheme] = useState("light");
+
+  // const toggleTheme = () => {
+  //   if (theme === "light") {
+  //     setTheme("dark");
+  //   } else {
+  //     setTheme("light");
+  //   }
+  // };
+
   return (
+    // <ThemeProvider theme={lightTheme}>
     <>
+      {/* <GlobalStyles /> */}
       <Header />
       <GameBoard />
       {/* <Modal /> */}
       <Footer />
     </>
+    // </ThemeProvider>
   );
 };
 
