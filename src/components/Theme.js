a23b093d0e22e777a4bd83d948493b3737138193
Yasya Mashnenko <yasyamashnenko@MacBook-Air-Daniil.local>
2021-02-25 14:26:13 +0100

    theme-mode draft

---
+++ src/components/Theme.js
@@ -0,0 +1,13 @@
+export const lightTheme = {
+  body: "#ffe227",
+  text: "#121013",
+  toggleBorder: "#FFF",
+  gradient: "linear-gradient(#39598A, #79D7ED)",
+};
+
+export const darkTheme = {
+  body: "#121013",
+  text: "#FAFAFA",
+  toggleBorder: "#6B8096",
+  gradient: "linear-gradient(#091236, #1E215D)",
+};
