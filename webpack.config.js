fea8eb86d4d7e227f157d6d17dcf8991205905ee
Yasya Mashnenko <yasyamashnenko@MacBook-Air-Daniil.local>
2021-02-25 14:03:49 +0100

    add ASSET_PATH

--- webpack.config.js
+++ webpack.config.js
@@ -2,12 +2,14 @@ const webpack = require("webpack");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
 
 const port = process.env.PORT || 3000;
+const ASSET_PATH = process.env.ASSET_PATH || "/";
 
 module.exports = {
   mode: "development",
   entry: "./src/index.js",
   output: {
     filename: "bundle.[hash].js",
+    publicPath: ASSET_PATH,
   },
   devtool: "inline-source-map",
   module: {
@@ -39,6 +41,9 @@ module.exports = {
     new HtmlWebpackPlugin({
       template: "public/index.html",
       // favicon: "public/favicon.ico",
+    }),
+    new webpack.DefinePlugin({
+      "process.env.ASSET_PATH": JSON.stringify(ASSET_PATH),
     }),
   ],
   devServer: {
