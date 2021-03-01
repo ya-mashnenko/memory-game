a962fe84623ee0fcfa5bf4fce5078bc6dea9d2d9
Yasya Mashnenko <yasyamashnenko@MacBook-Air-Daniil.local>
2021-03-01 20:43:16 +0100

    delete commented code

--- src/components/Utilities.js
+++ src/components/Utilities.js
@@ -9,14 +9,11 @@ const imagesForLongGame = [
   { src: "https://picsum.photos/200/250?random=4" },
 ];
 
-console.log(imagesForLongGame);
-
 const imagesForShortGame = imagesForLongGame.slice(0, 4);
 
 imagesForShortGame
   .sort(() => Math.random() - 0.5)
   .forEach((item) => (item.id = Math.round(Math.random() * 10000)));
-console.log(imagesForShortGame);
 
 imagesForLongGame
   .sort(() => Math.random() - 0.5)
