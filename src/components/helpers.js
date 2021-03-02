1307ffdc6e1bb06c9b10ba71b39fbd0e8bc84313
Yasya Mashnenko <yasyamashnenko@MacBook-Air-Daniil.local>
2021-03-02 13:20:56 +0100

    refactor: rename keys

--- src/components/helpers.js
+++ src/components/helpers.js
@@ -1,13 +1,10 @@
 import { v4 as uuid } from "uuid";
 
-const LONG_GAME_CARDS_AMOUNT = 16;
-const SHORT_GAME_CARDS_AMOUNT = 8;
-
 export const generateCards = (isLong) =>
-  [...Array(isLong ? LONG_GAME_CARDS_AMOUNT : SHORT_GAME_CARDS_AMOUNT).keys()]
+  [...Array(isLong ? 4 : 8).keys()]
     .map((item, i) => ({
       id: uuid(),
-      state: "FLIPPED",
+      state: "closed",
       src: `https://picsum.photos/200/250?random=${i % 2 ? i : i + 1}`,
     }))
     .sort(() => Math.random() - 0.5);
