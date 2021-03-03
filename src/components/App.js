d24299ce8d140f338d98095f26b1879c5032223f
Yasya Mashnenko <yasyamashnenko@MacBook-Air-Daniil.local>
2021-03-03 13:06:59 +0100

    feature: add local storage for gameLength

--- src/components/App.js
+++ src/components/App.js
@@ -3,18 +3,21 @@ import Footer from "./Footer";
 import Header from "./Header";
 import GameBoard from "./GameBoard";
 import { generateCards } from "./helpers";
-import useSound from "use-sound";
+import Cats from "./Cats";
+// import useSound from "use-sound";
 
 const App = () => {
   const localStorageCards = Array.from(
     JSON.parse(localStorage.getItem("cards"))
   );
   const localStorageSound = localStorage.getItem("soundOn") === "true";
-  const [isGameLong, setGameLength] = useState(true);
+  const localStorageGameLength = localStorage.getItem("gameLong") === "true";
+  const [isGameLong, setGameLength] = useState(localStorageGameLength || true);
   const [cards, setCards] = useState(
     localStorageCards || generateCards(isGameLong)
   );
   const [isSoundOn, setSound] = useState(localStorageSound || true);
+  const [timeOver, setTimeOver] = useState(false);
   const onNewGame = (isGameLong) => setCards(generateCards(isGameLong));
 
   useEffect(() => {
@@ -25,6 +28,10 @@ const App = () => {
     localStorage.setItem("soundOn", isSoundOn);
   }, [isSoundOn]);
 
+  useEffect(() => {
+    localStorage.setItem("gameLong", isGameLong);
+  }, [isGameLong]);
+
   return (
     <>
       <Header
@@ -33,13 +40,17 @@ const App = () => {
         isSoundOn={isSoundOn}
         setGameLength={setGameLength}
         isGameLong={isGameLong}
+        setTimeOver={setTimeOver}
+        timeOver={timeOver}
       />
       <GameBoard
         cards={cards}
         setCards={setCards}
         isSoundOn={isSoundOn}
         onNewGame={onNewGame}
+        timeOver={timeOver}
       />
+      <Cats />
       <Footer />
     </>
   );
